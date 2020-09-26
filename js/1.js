5b90dd34055943ef118d5a11fbefcf93039abb94
dbz <sumeshmeena2611@gmail.com>
2020-09-26 22:13:13 +0530

    update

---
+++ js/1.js
@@ -0,0 +1,15 @@
+/*var iframe = document.getElementsByTagName('iframe')[0];
+iframe.style.background = 'red';
+iframe.contentWindow.document.body.style.backgroundColor = 'green';
+*/
+
+/*var delayInMilliseconds = 500; // half a second
+
+setTimeout(function() {
+
+   var iframe = document.getElementsByTagName('iframe')[0];
+   iframe.style.background = 'green';
+   iframe.contentWindow.document.body.style.backgroundColor = 'green';
+
+}, delayInMilliseconds);
+*/
